0e2e94ac66d581dffb7dfd94ddd6c10e4340a041
sachinhub <Sachinkamblesk321@gamil.com>
2023-11-04 07:40:24 -0400

    all added

---
+++ Arraylist6.java
@@ -0,0 +1,37 @@
+//Multi Dimention ArrayList
+//...1 2 3 4 5
+//... 2 4 6 8 10
+//... 3 6 9 12 15 
+
+import java.util.ArrayList;
+
+public class Arraylist6 {
+    public static void main(String args[]) {
+        ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            list1.add(i * 1);
+            list2.add(i * 2);
+            list3.add(i * 3);
+        }
+
+        mainlist.add(list1);
+        mainlist.add(list2);
+        mainlist.add(list3);
+
+        System.out.println(mainlist);
+
+        for (int i = 0; i < mainlist.size(); i++) {
+            ArrayList<Integer> currentlist = mainlist.get(i);
+            for (int j = 0; j < currentlist.size(); j++) {
+                System.out.print(currentlist.get(j)+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
